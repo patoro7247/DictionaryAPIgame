f68288fc422b68f39a2e114087cc5a31ae141aff
Patrick Harding <patrickhardingr@gmail.com>
2021-05-03 16:18:27 -0700

    implemented getAntonymList, other initial game methods, updated README

--- README.txt
+++ README.txt
@@ -7,3 +7,8 @@ The user is awarded one point if they correctly give an antonym on the approved 
 The user will not be awarded any points if their word is not on the approved list
 The user will play until they reach a score of 5
 The user's overall score will be determined on the number of rounds it took them to finish
+
+Side notes:
+Not all words have antonyms
+If a word does have an antonym, its under entries>entry>lexemes>antonymsets>antonyms
+Api would sometimes append a single object list, as a result not able to turn antonymList into a set of unique antonyms, as you cannot turn a list into a set if the list contains a list
--- source.py
+++ source.py
@@ -2,20 +2,95 @@
 import requests
 import pprint
 import json
-
+from random_word import RandomWords
 
 #store the request in this r variable
-url = "https://lingua-robot.p.rapidapi.com/language/v1/entries/en/respect"
+url = "https://lingua-robot.p.rapidapi.com/language/v1/entries/en/"
+#put what word you want to look up after en/
+#r = RandomWords()
+totalScore = 0
 
 headers = {
     'x-rapidapi-key': "5da15bccffmsh6b27a6ff8f3b2f8p152169jsn23a9f2be1883",
     'x-rapidapi-host': "lingua-robot.p.rapidapi.com"
     }
 
-response = requests.request("GET", url, headers=headers)
+#response = requests.request("GET", url, headers=headers)
 
 #pprint.pprint(response.json()['entries'][0]['lexemes'][0]['antonymSets'][0])
+#.pprint(response.json())
 
-data = json.load(response.json())
+'''
+data = response.json()
+antonymList = []
 
-print(data['entries'])
+for i in range((len(data['entries'][0]['lexemes']))):
+    if 'antonymSets' in data['entries'][0]['lexemes'][i]:
+        for j in range((len(data['entries'][0]['lexemes'][i]['antonymSets']))):
+            for k in range((len(data['entries'][0]['lexemes'][i]['antonymSets'][j]['antonyms']))):
+                if isinstance(data['entries'][0]['lexemes'][i]['antonymSets'][j]['antonyms'][k], str):
+                    antonymList.append(data['entries'][0]['lexemes'][i]['antonymSets'][j]['antonyms'][k])
+
+            #print(data['entries'][0]['lexemes'][i]['antonymSets'][j]['antonyms'])
+        #antonymList.append(data['entries'][0]['lexemes'][i]['antonymSets'])
+        
+        print(antonymList)
+        uniqueAntonymList = set(antonymList)
+        
+'''
+
+
+def getAntonymList(word):
+    antonymList = []
+    newURL = url+word
+    response = requests.request("GET", newURL, headers=headers)
+    data = response.json()
+    for i in range((len(data['entries'][0]['lexemes']))):
+        if 'antonymSets' in data['entries'][0]['lexemes'][i]:
+            for j in range((len(data['entries'][0]['lexemes'][i]['antonymSets']))):
+                for k in range((len(data['entries'][0]['lexemes'][i]['antonymSets'][j]['antonyms']))):
+                    if isinstance(data['entries'][0]['lexemes'][i]['antonymSets'][j]['antonyms'][k], str):
+                        antonymList.append(data['entries'][0]['lexemes'][i]['antonymSets'][j]['antonyms'][k])
+
+    uniqueAntonymList = set(antonymList)
+    print(uniqueAntonymList)
+    #if len(uniqueAntonymList) == 0:
+    return uniqueAntonymList
+
+
+def intro():
+    print("The object of this game is to correctly guess an antonym for a random word")
+    print("Every correct answer will net you 1 point")
+    print("Once you receive 5 points, you win")
+    doesUserWanttoPlay = input("If you want to play, press any key, otherwise, exit the program")
+    if doesUserWanttoPlay:
+        startGame()
+
+
+def startGame():
+    global totalScore
+    r = RandomWords()
+    nextR = RandomWords()
+
+    randomWord = r.get_random_word(hasDictionaryDef="true")
+    getAntonymList(randomWord)
+    guess = input("Guess an antonym for: "+randomWord)
+    if guess in getAntonymList(randomWord):
+        print("Congratulations, you earned a point!")
+        totalScore += 1
+    else:
+        print("Sorry, that was wrong, try again with the next word")
+        nextRandomWord = nextR.get_random_word(hasDictionaryDef="true")
+        getAntonymList(nextRandomWord)
+
+''' Old getAntonymList def
+def getAntonymList(word):
+    newURL = url+word
+    response = requests.request("GET", newURL, headers=headers)
+    data = response.json()['entries'][0]['lexemes'][0]['antonymSets'][0]
+    responseDictionary = json.loads(response.json())
+    print(responseDictionary)
+    #print(data)
+'''
+intro()
+startGame()
