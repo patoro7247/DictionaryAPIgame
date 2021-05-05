65660da896d002f3bb14d539d67597dcd98b541c
Patrick Harding <patrickhardingr@gmail.com>
2021-05-05 01:13:06 -0700

    Cleaned up source file, refactored code into smaller methods, more easy to read, added verification so that only words that have antonyms are brought up for user to guess

--- source.py
+++ source.py
@@ -9,35 +9,12 @@ url = "https://lingua-robot.p.rapidapi.com/language/v1/entries/en/"
 #put what word you want to look up after en/
 #r = RandomWords()
 totalScore = 0
+totalRounds = 0
 
 headers = {
     'x-rapidapi-key': "5da15bccffmsh6b27a6ff8f3b2f8p152169jsn23a9f2be1883",
     'x-rapidapi-host': "lingua-robot.p.rapidapi.com"
     }
-
-#response = requests.request("GET", url, headers=headers)
-
-#pprint.pprint(response.json()['entries'][0]['lexemes'][0]['antonymSets'][0])
-#.pprint(response.json())
-
-'''
-data = response.json()
-antonymList = []
-
-for i in range((len(data['entries'][0]['lexemes']))):
-    if 'antonymSets' in data['entries'][0]['lexemes'][i]:
-        for j in range((len(data['entries'][0]['lexemes'][i]['antonymSets']))):
-            for k in range((len(data['entries'][0]['lexemes'][i]['antonymSets'][j]['antonyms']))):
-                if isinstance(data['entries'][0]['lexemes'][i]['antonymSets'][j]['antonyms'][k], str):
-                    antonymList.append(data['entries'][0]['lexemes'][i]['antonymSets'][j]['antonyms'][k])
-
-            #print(data['entries'][0]['lexemes'][i]['antonymSets'][j]['antonyms'])
-        #antonymList.append(data['entries'][0]['lexemes'][i]['antonymSets'])
-        
-        print(antonymList)
-        uniqueAntonymList = set(antonymList)
-        
-'''
 
 
 def getAntonymList(word):
@@ -45,16 +22,24 @@ def getAntonymList(word):
     newURL = url+word
     response = requests.request("GET", newURL, headers=headers)
     data = response.json()
-    for i in range((len(data['entries'][0]['lexemes']))):
-        if 'antonymSets' in data['entries'][0]['lexemes'][i]:
-            for j in range((len(data['entries'][0]['lexemes'][i]['antonymSets']))):
-                for k in range((len(data['entries'][0]['lexemes'][i]['antonymSets'][j]['antonyms']))):
-                    if isinstance(data['entries'][0]['lexemes'][i]['antonymSets'][j]['antonyms'][k], str):
-                        antonymList.append(data['entries'][0]['lexemes'][i]['antonymSets'][j]['antonyms'][k])
 
+
+    if 'lexemes' in data['entries'][0]:
+        if len(data['entries'][0]['lexemes']) > 0:
+            for i in range((len(data['entries'][0]['lexemes']))):
+                if 'antonymSets' in data['entries'][0]['lexemes'][i]:
+                    for j in range((len(data['entries'][0]['lexemes'][i]['antonymSets']))):
+                        for k in range((len(data['entries'][0]['lexemes'][i]['antonymSets'][j]['antonyms']))):
+                            if isinstance(data['entries'][0]['lexemes'][i]['antonymSets'][j]['antonyms'][k], str):
+                                antonymList.append(data['entries'][0]['lexemes'][i]['antonymSets'][j]['antonyms'][k])
+    else:
+        startRound()
     uniqueAntonymList = set(antonymList)
-    print(uniqueAntonymList)
-    #if len(uniqueAntonymList) == 0:
+    #print(uniqueAntonymList)
+    if len(uniqueAntonymList) == 0:
+        return 0
+
+    print(antonymList)
     return uniqueAntonymList
 
 
@@ -64,33 +49,64 @@ def intro():
     print("Once you receive 5 points, you win")
     doesUserWanttoPlay = input("If you want to play, press any key, otherwise, exit the program")
     if doesUserWanttoPlay:
-        startGame()
+        startRound()
 
 
-def startGame():
+def verifyWordHasAntonym(word):
+    antonymList = []
+    newURL = url + word
+    response = requests.request("GET", newURL, headers=headers)
+    data = response.json()
+    if 'lexemes' in data['entries'][0]:
+        if len(data['entries'][0]['lexemes']) > 0:
+            for i in range((len(data['entries'][0]['lexemes']))):
+                if 'antonymSets' in data['entries'][0]['lexemes'][i]:
+                    for j in range((len(data['entries'][0]['lexemes'][i]['antonymSets']))):
+                        for k in range((len(data['entries'][0]['lexemes'][i]['antonymSets'][j]['antonyms']))):
+                            if isinstance(data['entries'][0]['lexemes'][i]['antonymSets'][j]['antonyms'][k], str):
+                                antonymList.append(data['entries'][0]['lexemes'][i]['antonymSets'][j]['antonyms'][k])
+    else:
+        startRound()
+    uniqueAntonymList = set(antonymList)
+
+    if len(uniqueAntonymList) > 0:
+        return True
+
+    else:
+        return False
+
+
+def userGuess(antonymset, theRandomWordGiven):
+    global totalScore
+    global totalRounds
+
+    print("Guess an antonym for: "+theRandomWordGiven)
+    userInput = input()
+    if userInput in antonymset:
+        print("Congratulations, that was correct\n")
+        totalScore += 1
+        totalRounds += 1
+        startRound()
+
+    else:
+        print("Sorry, that was the wrong answer, try again.")
+        totalRounds += 1
+        startRound()
+
+
+def startRound():
     global totalScore
     r = RandomWords()
     nextR = RandomWords()
 
     randomWord = r.get_random_word(hasDictionaryDef="true")
-    getAntonymList(randomWord)
-    guess = input("Guess an antonym for: "+randomWord)
-    if guess in getAntonymList(randomWord):
-        print("Congratulations, you earned a point!")
-        totalScore += 1
-    else:
-        print("Sorry, that was wrong, try again with the next word")
-        nextRandomWord = nextR.get_random_word(hasDictionaryDef="true")
-        getAntonymList(nextRandomWord)
 
-''' Old getAntonymList def
-def getAntonymList(word):
-    newURL = url+word
-    response = requests.request("GET", newURL, headers=headers)
-    data = response.json()['entries'][0]['lexemes'][0]['antonymSets'][0]
-    responseDictionary = json.loads(response.json())
-    print(responseDictionary)
-    #print(data)
-'''
+    if verifyWordHasAntonym(randomWord):
+        userGuess(getAntonymList(randomWord), randomWord)
+    else:
+        startRound()
+
+
+
 intro()
-startGame()
+startRound()
